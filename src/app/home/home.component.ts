d8a84aa5f64a1b3f8f5c609f2da377b17b95cd76
Albin18 <albianjose0424@gmail.com>
2024-05-05 01:39:48 -0400

    styles-home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CartComponent } from '../cart/cart.component';
+import { ItemCart } from '../models/itemCart';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CartComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  @Input() items: ItemCart[] = [];
 
+
+  @Input() total: number = 0;
+
+  @Input() products: Product[] = [];
 }
